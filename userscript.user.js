830239b527e976bdc662b4e7130f5b82fec238d8
Derpinias <102878526+Derpinias@users.noreply.github.com>
2022-04-02 22:56:04 +0200

    Add files via upload

---
+++ userscript.user.js
@@ -0,0 +1,24 @@
+// ==UserScript==
+// @name         Hololive combo
+// @namespace    http://tampermonkey.net/
+// @version      0.1
+// @description  Spread the love
+// @author       oralekin
+// @match        https://hot-potato.reddit.com/embed*
+// @icon         https://www.google.com/s2/favicons?sz=64&domain=reddit.com
+// @grant        none
+// ==/UserScript==
+if (window.top !== window.self) {
+    window.addEventListener('load', () => {
+            document.getElementsByTagName("mona-lisa-embed")[0].shadowRoot.children[0].getElementsByTagName("mona-lisa-canvas")[0].shadowRoot.children[0].appendChild(
+        (function () {
+            const i = document.createElement("img");
+            i.src = "https://raw.githubusercontent.com/gaudi99/hololive-template-picture/main/hololive%20placetemplate-1.png.png";
+            i.style = "position: absolute;left: 0;top: 0;image-rendering: pixelated;width: 2000px;height: 1000px;";
+            console.log(i);
+            return i;
+        })())
+
+    }, false);
+
+}
